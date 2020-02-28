6e39f64f6cc2492abf6ef23db728dd7cdab3250f
Igor Prykhodzka <Kvait@bk.ru>
2020-02-28 12:45:26 +0300

    first commit

---
+++ app/src/main/java/com/example/hwandroid52/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.hwandroid52;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import androidx.appcompat.widget.Toolbar;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+
+        Intent intentNotes = new Intent(MainActivity.this, NotesActivity.class);
+        startActivity(intentNotes);
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_open_notes) {
+            Toast.makeText(MainActivity.this, R.string.txtOpenNote, Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
